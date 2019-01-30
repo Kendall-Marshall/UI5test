a3bf38fd006663889527c7d633a874cfc6bbbc8c
Kendall Marshall <kendall@zag.team>
2019-01-30 03:38:46 +0000

    Initial Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+	"use strict";
+
+	return Controller.extend("Test.Test.controller.View1", {
+		onInit : function () {
+         // set data model on view
+			var oData = {
+				recipient : {
+				   name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+    	},
+		onShowHello : function () {
+	         // show a native JavaScript alert
+	         MessageToast.show("Hello");
+	         //alert("Hello World");
+	     }
+	});
+});
